b45eb5ce5ef0e7a9ad9305f5d634eb35f29eec46
clacaze <charlie.lacaze@cegedim-activ.com>
2020-09-08 11:05:52 +0200

    add comments about the objective

--- src/components/Books/BooksMiddleware.js
+++ src/components/Books/BooksMiddleware.js
@@ -11,6 +11,10 @@ const BOOKS_URL = "https://www.googleapis.com/books/v1/volumes?q=bookToSearch&&m
 /* ********        MIDDLEWARE      ******** */
 /* **************************************** */
 
+// Objectif : décrire le flow complet permettant de récupérer des livres, en activant les spinners.
+// il s'agit là de réagir aux bonnes actions au travers d'un processus événementiel. Il faut donc mapper certaines
+// actions spécifiques à d'autres.
+
 /* ************************************* */
 /* ********       EXPORTS       ******** */
 /* ************************************* */
--- src/middleware/core/Api.js
+++ src/middleware/core/Api.js
@@ -10,6 +10,11 @@
 /* ********        MIDDLEWARE      ******** */
 /* **************************************** */
 
+// Objectif : rajouter ici l'appel à l'api.
+// L'objectif est d'avoir un code indépendant de tout contexte, ou l'on récupère les paramètres nécessaires à l'appel API
+// Penser à renvoyer les bonnes actions en cas de succes ou de fail de la requête.
+// Un simple fetch / then / catch suffira.
+
 /* ************************************* */
 /* ********       EXPORTS       ******** */
 /* ************************************* */
